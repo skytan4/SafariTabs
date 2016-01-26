01d855e65eb6486b43941fc78702f934ecfae9b7
skytan4 <skylertanner@live.com>
2016-01-26 10:55:43 -0700

    Initial Commit

---
+++ SafariTabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafariTabs
+//
+//  Created by Skyler Tanner on 1/26/16.
+//  Copyright © 2016 The Church of Jesus Christ of Latter-day Saints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
